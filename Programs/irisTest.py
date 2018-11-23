d2beecfd431e87355c6da5d7144a7a90555ddebb
harishSherlock <44371227+harishSherlock@users.noreply.github.com>
2018-11-23 14:37:25 +0530

    Update irisTest.py

--- Programs/irisTest.py
+++ Programs/irisTest.py
@@ -9,11 +9,11 @@ from sklearn.datasets import load_iris
 from sklearn import tree
 
 iris=load_iris()
-#for i in range(len(iris.target)):
-#    print("Example: %d: Label: %s, Features: %s" % (i+1,iris.target_names[iris.target[i]],iris.data[i]))
+for i in range(len(iris.target)):
+    print("Example: %d: Label: %s, Features: %s" % (i+1,iris.target_names[iris.target[i]],iris.data[i]))
 
 flowerClassifier=tree.DecisionTreeClassifier();
 flowerClassifier=flowerClassifier.fit(iris.data,iris.target)
 
 print(iris.target_names[flowerClassifier.predict([[6.6,2.4,4.9,2.1]])]) #just try giving random values inside predict and check if the
-# -results are correct   
+# -results are correct   with the above printed examples
